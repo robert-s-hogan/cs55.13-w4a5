9826a85b6e6da361b69d4d48158508bf6e497284
robert.s.hogan@icloud.com <robert.s.hogan@icloud.com>
2025-10-01 05:23:08 -0700

    implemented getStaticProps

--- assignment5/nextjs-blog/lib/posts.js
+++ assignment5/nextjs-blog/lib/posts.js
@@ -3,6 +3,7 @@ import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
 
+// Added pages path to read posts from pages/posts
 const postsDirectory = path.join(process.cwd(), "pages", "posts");
 
 export function getSortedPostsData() {
@@ -33,4 +34,45 @@ export function getSortedPostsData() {
       return -1;
     }
   });
+}
+
+// Added getAllPostIds function to return ids for dynamic pages
+export function getAllPostIds() {
+  const fileNames = fs.readdirSync(postsDirectory);
+
+  // Returns an array that looks like this:
+  // [
+  //   {
+  //     params: {
+  //       id: 'ssg-ssr'
+  //     }
+  //   },
+  //   {
+  //     params: {
+  //       id: 'pre-rendering'
+  //     }
+  //   }
+  // ]
+  return fileNames.map((fileName) => {
+    return {
+      params: {
+        id: fileName.replace(/\.md$/, ""),
+      },
+    };
+  });
+}
+
+// Function to return post data based on id
+export function getPostData(id) {
+  const fullPath = path.join(postsDirectory, `${id}.md`);
+  const fileContents = fs.readFileSync(fullPath, "utf8");
+
+  // Use gray-matter to parse the post metadata section
+  const matterResult = matter(fileContents);
+
+  // Combine the data with the id
+  return {
+    id,
+    ...matterResult.data,
+  };
 }
